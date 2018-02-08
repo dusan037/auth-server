14bea36ef5d7709d70a4f1e4bb995a95b0d74df7
Dusan Simonovic <c.dsimonovic@gogoair.com>
2018-02-08 09:53:02 +0100

    auth final

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,6 +7,12 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function (req, res, next) {
+  // User has already their email and password auth'd
+  // We just need to give them a token
+  res.send({ token: tokenForUser(req.user) });
+}
+
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
@@ -34,7 +40,7 @@ exports.signup = function(req, res, next) {
       if(err) { return next(err); }
 
       // Respond to request indicating the user was created
-      res.json({token: tokenForUser(usern)});
+      res.json({token: tokenForUser(user)});
     });
   });
 };
